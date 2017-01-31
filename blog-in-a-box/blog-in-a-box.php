965266d6845e466f08ea6a88bed7c5188acc4ff1
Marcus Kazmierczak <marcus@mkaz.com>
2017-01-31 02:32:48 -0800

    Add button to ajax call script

--- blog-in-a-box/blog-in-a-box.php
+++ blog-in-a-box/blog-in-a-box.php
@@ -8,11 +8,31 @@ Version: 0.1
 
 add_action('admin_menu', 'biab_setup_menu');
 
-function biab_setup_menu(){
+function biab_setup_menu() {
 	add_menu_page( 'Blog in a Box Admin Page', 'BIAB 📦', 'manage_options', 'biab-plugin', 'biab_init' );
 }
 
-function biab_init(){
-	echo "<h1>Put Your Blog in a Box</h1>";
-}
+function biab_init() { ?>
+	<h1> Put Your Blog in a Box </h1>
+	<script>
+		function take_photo() {
+			jQuery.post( "admin-post.php", { action: "biab_take_photo" } ).done(
+				function( data ) {
+					jQuery( ".result" ).html( data );
+				}
+			);
+			return false;
+		}
+	</script>
+	<form>
+		<button onClick="return take_photo()"> Take Photo </button>
+	</form>
 
+	<div class="result"></div>
+<?php
+}
+add_action( 'admin_post_biab_take_photo', 'biab_take_photo' );
+
+function biab_take_photo() {
+	echo "<h3> Click! </h3> ";
+}
